51511bf03e575d2c91002869e05ac75c35bec3e7
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-09-13 14:45:47 +0300

    start

---
+++ DjangoProject/news/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+
+admin.site.register(News, NewsAdmin)
