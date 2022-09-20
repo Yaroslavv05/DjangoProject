c626c47687b20ca16a48f9d1ae868115d93f7714
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-09-20 14:55:03 +0300

    update

--- DjangoProject/news/views.py
+++ DjangoProject/news/views.py
@@ -4,12 +4,10 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей', 'categoris': categories,})
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей', })
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categoris': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category, })
 
