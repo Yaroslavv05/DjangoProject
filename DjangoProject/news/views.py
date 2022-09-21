922a740ed30ce9b46a37d98be9f434a0c9b141fe
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-09-21 10:54:20 +0300

    update

--- DjangoProject/news/views.py
+++ DjangoProject/news/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import News, Category
+
 
 def index(request):
     news = News.objects.order_by('-created_at')
     return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей', })
 
+
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category, })
+
+
+def view_news(requesr, news_id):
+    #news_item = News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(requesr, 'news/view_news.html', {'news_item': news_item})
 
