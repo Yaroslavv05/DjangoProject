38f8c1e71b07a10a8845015da5afad255557a447
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-09-22 23:14:11 +0300

    update

--- DjangoProject/news/forms.py
+++ DjangoProject/news/forms.py
@@ -1,9 +1,13 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    content = forms.CharField(label='Текст', widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
-    is_published = forms.BooleanField(label='Опубликовано?')
-    category = forms.ModelChoiceField(empty_label='Выберите категорию', queryset=Category.objects.all(), label='Категории', widget=forms.Select(attrs={'class': 'form-control'}))
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
