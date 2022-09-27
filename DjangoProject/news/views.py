ccbeafd413fe43b99954291505cd8d80199060d5
Limb <88242725+Yaroslavv05@users.noreply.github.com>
2022-09-27 22:57:56 +0300

    update

--- DjangoProject/news/views.py
+++ DjangoProject/news/views.py
@@ -15,7 +15,7 @@ class HomeNews(ListView):
         return extra_context
 
     def get_queryset(self):
-        return News.objects.filter(is_published=True)
+        return News.objects.filter(is_published=True).select_related('category')
 
 
 class NewsByCategory(ListView):
@@ -42,32 +42,5 @@ class ViewNews(DeleteView):
 class CreateNews(CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
-'''''
-def index(request):
-    news = News.objects.order_by('-created_at')
-    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей', })
 
-
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category, })
-
-
-def view_news(requesr, news_id):
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(requesr, 'news/view_news.html', {'news_item': news_item})
-
-
-
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
-            news = form.save()
-            return redirect(news)
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
-'''''
 
